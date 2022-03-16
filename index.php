e35ee7044f1b5c53374476c9f2cfab7050e5a69b
AliceColombari <colombari.23@gmail.com>
2022-03-16 13:21:08 +0100

    base

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// provate ad immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche.
+// L’e-commerce vende prodotti per gli animali.
+// I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+// L’utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+// Il pagamento avviene con la carta di credito, che non deve essere scaduta.
+
+
+
+?>
